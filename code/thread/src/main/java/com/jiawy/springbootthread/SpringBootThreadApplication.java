d342c6c220cfb59196b5265f1571c7998f0f760c
jiawenyao <jiawenyao401@sina.com>
2021-01-04 17:26:26 +0800

    --

--- code/thread/src/main/java/com/jiawy/springbootthread/SpringBootThreadApplication.java
+++ code/thread/src/main/java/com/jiawy/springbootthread/SpringBootThreadApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.jiawy.springbootthread.persistence")
+@MapperScan("com.jiawy.springbootthread.persistence.Mapper")
 @SpringBootApplication
 public class SpringBootThreadApplication {
 
--- code/thread/src/main/java/com/jiawy/springbootthread/persistence/service/impl/UserServiceImpl.java
+++ code/thread/src/main/java/com/jiawy/springbootthread/persistence/service/impl/UserServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl extends UserService {
+public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserMapper userMapper;
